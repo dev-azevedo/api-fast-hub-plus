d68dc5180d6866bbbf68dee407b3e2ade2c4d653
Jhonatan Azevedo <dev.azevedo@outlook.com>
2025-02-17 12:01:08 -0300

    feat: event service class

--- src/modules/events/event.service.ts
+++ src/modules/events/event.service.ts
@@ -1,5 +1,41 @@
+import { Event } from "@prisma/client";
+import EventRepository from "./event.repository.js";
+
 class EventService {
-    
+    private readonly _repository: EventRepository;
+
+    constructor(repository: EventRepository) {
+        this._repository = repository;
+    }
+
+    public findAll = async (): Promise<Event[]> => {
+        return await this._repository.findAll();
+    }
+
+    public findById = async (id: string): Promise<Event> => {
+        const eventOnDb = await this._repository.findById(id);
+
+        if (!eventOnDb)
+            throw new Error("Event not found");
+
+        return eventOnDb;
+    }
+
+    public create = async (event: Event): Promise<Event> => {
+        const eventOnDb = await this._repository.createEvent(event);
+
+        return eventOnDb;
+    }
+
+    public update = async (event: Event): Promise<Event> => {
+        const eventOnDb = await this._repository.updateEvent(event);
+
+        return eventOnDb;
+    }
+
+    public deactiveEvent = async (id: string): Promise<void> => {
+        await this._repository.deactiveEvent(id);
+    }
 }   
 
 export default EventService;
