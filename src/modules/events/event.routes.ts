c0ac90fbe6d800c2205314cc157919165ed1fb1c
Jhonatan Azevedo <dev.azevedo@outlook.com>
2025-02-17 12:13:35 -0300

    feat: add event routes

--- src/modules/events/event.routes.ts
+++ src/modules/events/event.routes.ts
@@ -1,4 +1,16 @@
-const eventRoutes = "";
+import { Router } from "express";
+
+import EventController from "./event.controller.js";
+import { authUser } from "./../../shared/middlewares/auth.middleware.js";
+
+const eventRoutes = Router();
+const eventController = new EventController();
+
+eventRoutes.get("/events", authUser, eventController.findAll);
+eventRoutes.get("/events/:id", authUser, eventController.findById);
+eventRoutes.post("/events", authUser, eventController.create);
+eventRoutes.put("/events/", authUser, eventController.update);
+eventRoutes.patch("/events/:id", authUser, eventController.deactive);
 
 
 export default eventRoutes;
