dd31018f669a3cca62fe22fa202b1f74d6a3fb80
Jhonatan Azevedo <dev.azevedo@outlook.com>
2025-02-17 16:53:47 -0300

    feat: validation dtio in routes reservation

--- src/modules/reservations/reservation.routes.ts
+++ src/modules/reservations/reservation.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { authUser } from "./../../shared/middlewares/auth.middleware.js";
 import ReservationController from "./reservation.controller.js";
+import { validateCreateReservationDto, validateUpdateReservationDto } from "./reservation.validation.js";
 
 
 const reservationRoutes = Router();
@@ -9,8 +10,8 @@ const reservationController = new ReservationController();
 
 reservationRoutes.get("/reservations", authUser, reservationController.findAll);
 reservationRoutes.get("/reservations/:id", authUser, reservationController.findById);
-reservationRoutes.post("/reservations", authUser, reservationController.create);
-reservationRoutes.put("/reservations", authUser, reservationController.update);
+reservationRoutes.post("/reservations", authUser, validateCreateReservationDto, reservationController.create);
+reservationRoutes.put("/reservations", authUser, validateUpdateReservationDto, reservationController.update);
 reservationRoutes.patch("/reservations/deactive/:id", authUser, reservationController.deactive);
 
 export default reservationRoutes;
